4161a35c07e044ed3dbbc4f6a8f9b6f77e59c661
JeanBaptisteBENETTI <jean-baptiste.benetti@etud.univ-jfc.fr>
2023-01-20 11:15:43 +0100

    First commit.

--- src/main/java/comptoirs/service/CommandeService.java
+++ src/main/java/comptoirs/service/CommandeService.java
@@ -1,7 +1,10 @@
 package comptoirs.service;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import comptoirs.entity.Ligne;
+import comptoirs.entity.Produit;
 import org.springframework.stereotype.Service;
 
 import comptoirs.dao.ClientRepository;
@@ -63,7 +66,18 @@ public class CommandeService {
      */
     @Transactional
     public Commande enregistreExpédition(Integer commandeNum) {
-        // TODO : implémenter ce service métier
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        var commande = commandeDao.findById(commandeNum).orElseThrow();
+        if (commande.getEnvoyeele() == null) {
+            commande.setEnvoyeele(LocalDate.now());
+            Commande c = null;
+            for (Ligne l : c.getLignes()) {
+                Produit p = l.getProduit();
+                p.setUnitesEnStock(p.getUnitesEnStock() - l.getQuantite());
+            }
+        } else {
+            throw new IllegalArgumentException("la commande est déjà enregistré");
+        }
+
+        return commande;
     }
 }
